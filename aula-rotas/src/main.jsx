fc5a73eeb67253c1ae8e9b97325dcfbf5c7eb793
VitorKubica <vitor05kubica12@gmail.com>
2023-09-13 10:43:54 -0300

    criação e aplicação do router(config das rotas)

--- aula-rotas/src/main.jsx
+++ aula-rotas/src/main.jsx
@@ -8,8 +8,31 @@ import Produtos from '../routes/Produtos/index.jsx'
 import Error from '../routes/Error/index.jsx'
 import EditarProdutos from '../routes/EditarProdutos/index.jsx'
 
+const router = createBrowserRoutes([
+  {
+    path : "/",
+    element:<App/>,
+    errorElement:<Error/>,
+    children:[
+      {
+        path : "/",
+        element:<Home/>,
+      },
+      {
+        path : "/produtos",
+        element:<Produtos/>,
+      },
+      {
+        path : "/produtos/editar/:id",
+        element:<EditarProdutos/>,
+      }
+    ]
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
+
