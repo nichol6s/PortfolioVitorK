1862a9edaedd16dcb4fb8b8d19e9395d409881ac
VitorKubica <vitor05kubica12@gmail.com>
2023-09-13 11:32:37 -0300

    atualização main

--- aula-rotas/src/main.jsx
+++ aula-rotas/src/main.jsx
@@ -2,37 +2,41 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { createBrowserRoutes, RouterProvider } from 'react-router-dom'
+import {createBrowserRouter, RouterProvider, Navigate} from 'react-router-dom'
 import Home from '../routes/Home/index.jsx'
-import Produtos from '../routes/Produtos/index.jsx'
 import Error from '../routes/Error/index.jsx'
 import EditarProdutos from '../routes/EditarProdutos/index.jsx'
+import Produtos from '../routes/Produtos/index.jsx'
 
-const router = createBrowserRoutes([
+ 
+
+const router = createBrowserRouter([
   {
-    path : "/",
-    element:<App/>,
-    errorElement:<Error/>,
-    children:[
+    path: "/",
+    element: <App/>,
+    errorElement: <Error/>,
+    children: [
       {
-        path : "/",
-        element:<Home/>,
+        path: "/",
+        element: <Home/>
       },
       {
-        path : "/produtos",
-        element:<Produtos/>,
+        path: "/produtos",
+        element: <Produtos/>
       },
       {
-        path : "/produtos/editar/:id",
-        element:<EditarProdutos/>,
+        path: "/produtos/editar/:id",
+        element: <EditarProdutos/>
+      },
+      {
+        path: "/antiga",
+        element: <Navigate to="/"/>
       }
     ]
-  }
-])
+}])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+  <RouterProvider router={router} />
   </React.StrictMode>,
 )
-
