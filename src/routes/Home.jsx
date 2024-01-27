99d74fc7c30c526d57ac5ce25fc285302d2b65b1
VitorKubica <vitor05kubica12@gmail.com>
2024-01-27 04:55:54 -0300

    alterações

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,9 +1,28 @@
 import './Home.scss';
+import { Link } from "react-router-dom";
+import fotoap from '/img/fotoap.webp'
+import setad from '/LogoSimples.svg'
 
 export default function Home() {
   return (
     <main className='conteudo'>
-      
+      <section className='apresentacao'>
+        <div className='textoap'>
+          <h1>DESENVOLVEDOR FULL STACK</h1>
+          <p>Portifólio profissional.</p>
+          <Link to={""}>
+            <p>Ver mais</p>
+            <div className='seta'>
+              <div className='setacorpo'></div>
+              <div className='setacabeca'></div>
+            </div>
+          </Link>
+        </div>
+        <div className='imagem'>
+          <img src={fotoap}/>
+        </div>
+      </section>
     </main>
   )
 }
+  
