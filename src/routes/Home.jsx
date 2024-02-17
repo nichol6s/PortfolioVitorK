a121c4bc767fcdcb9c7d691c43140a99c586465a
VitorKubica <vitor05kubica12@gmail.com>
2024-02-17 12:04:14 -0300

    até a 3 section responsiva

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -139,12 +139,13 @@
           <div className='conteudosobremim' >
             <h1>SOBRE MIM</h1>
             <p>Olá! 
-              <br/> Sou o Vitor, tenho 19 anos e sou estudante de ADS na FIAP. Com habilidades como organização, postura ativa, boa comunicação e relacionamento interpessoal, raciocínio lógico e pensamento analítico. <br/>  Vivendo o mundo da programação, sempre em busca de aprender.</p>
+              <br/> Sou o Vitor, tenho 19 anos e sou estudante de ADS na FIAP. Tenho habilidades como organização, postura ativa, boa comunicação e relacionamento interpessoal, raciocínio lógico e pensamento analítico. <br/>  Vivendo o mundo da programação, sempre em busca de aprender.</p>
             <div className='botoes'>
               <button className="buttoncontato" type="button" onClick={() => handleClick('ref2')}>
                 <span className="buttoncontato__text">Contato</span>
               </button>
-              <button className="buttondwnld" type="button" onClick={handleDownload}>
+              {/* onClick={handleDownload} */}
+              <button className="buttondwnld" type="button" >
                 <span className="buttondwnld__text">Download CV </span>
                 <span className="buttondwnld__icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 35 35" id="bdd05811-e15d-428c-bb53-8661459f9307" data-name="Layer 2" className="svg"><path d="M17.5,22.131a1.249,1.249,0,0,1-1.25-1.25V2.187a1.25,1.25,0,0,1,2.5,0V20.881A1.25,1.25,0,0,1,17.5,22.131Z"></path><path d="M17.5,22.693a3.189,3.189,0,0,1-2.262-.936L8.487,15.006a1.249,1.249,0,0,1,1.767-1.767l6.751,6.751a.7.7,0,0,0,.99,0l6.751-6.751a1.25,1.25,0,0,1,1.768,1.767l-6.752,6.751A3.191,3.191,0,0,1,17.5,22.693Z"></path><path d="M31.436,34.063H3.564A3.318,3.318,0,0,1,.25,30.749V22.011a1.25,1.25,0,0,1,2.5,0v8.738a.815.815,0,0,0,.814.814H31.436a.815.815,0,0,0,.814-.814V22.011a1.25,1.25,0,1,1,2.5,0v8.738A3.318,3.318,0,0,1,31.436,34.063Z"></path></svg></span>
               </button>
